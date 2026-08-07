926f40c1aa57a18981a68775dd2eb422ef85078a
skfoa <145113954+skfoa@users.noreply.github.com>
2026-08-07 17:39:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ except ImportError:
 
 # 配置区域
 RENEW_URLS = [
-    "https://host2play.gratis/server/renew?i=6666666-6666-6666-6666-66666666666",
+    "https://host2play.gratis/server/renew?i=86bab149-aec3-4eb0-8bf9-332ff88e3eec",
     # 添加更多链接
 ]
 
